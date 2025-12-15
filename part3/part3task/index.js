ca000af6bd3821b3b7fe8ffd19fc87b205fdd510
Chorongie <yiln569@aucklanduni.ac.nz>
2025-12-15 16:22:00 +1300

    task 3.1 - 3.6

---
+++ part3/part3task/index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = notes.find(note => note.id === id)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  if (notes.some(note => note.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  } 
+
+  const newNote = {
+    id: Math.floor(Math.random() * 10000),
+    name: body.name,
+    number: body.number
+  }
+
+  notes = notes.concat(newNote)
+  response.json(newNote)
+})
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
